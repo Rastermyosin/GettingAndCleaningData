2c6ef3fdd5f0a57c8b4e92bf1a0540e782b31059
Rastermyosin <kjon.oneill@gmail.com>
2015-02-22 16:26:50 -0700

    Added run_analysis.R and updated the README
    Added run_analysis.R. Will auto-run when sourced and will output a file
    called "TidyData.txt".
    
    Updated README.md to include instructions and a description of the
    run_analysis.R script
    
    -Kevin O'Neill

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # GettingAndCleaningData
 Programming Assignment for Coursera's Getting and Cleaning Data class from Johns Hopkins
+
+### run_analysis
+
+When sourced into R, this script will extract and manipulate data from a Samsung phone study. This code will output a text file entitled "TidyData.txt" which will contain the mean and standard deviation for each activity for each subject.
+
+In order to use this data you must:
+* Have the Samsung Phone data in your working directory (not within sub folders)
+* Have the data.table and dpyr packages installed
+* When you source("run_analysis.R") the script will auto-run
+
+#### Variables
+
+##### Files Names
+Each of the following variables contains the file name of a particular piece of the overall dataset.
+
+'''R
+xTrainName # File name of the training data (X_train.txt)
+yTrainName # File name of the training activity data (Y_train.txt)
+
+xTestName # File name of the test data (X_test.txt)
+yTestName # File name of the test activity data (Y_test.txt)
+
+subjectTrainName # File name of the training subject IDs (subject_train.txt)
+subjectTestName  # File name of the test subject IDs (subject_test.txt)
+
+activityName # File name of the activity list (activity_labels.txt)
+featureName  # File name of the feature list (features.txt)
+'''
+
+##### Indices
+Each of the following contains important indices in the dataset
+
+'''R
+featureColumns # Contains the column index for each mean and std measurement
+'''
+
+##### Data Containers
+Each of the following holds on to data gathered from the data files
+'''R
+featureLabels # Contains the strings for each feature name (tBodyAcc.mean.X,...)
+
+trainX # Contains all of the training data for each mean and std measurement
+trainY # Contains all of the training activity index data
+
+testX # Contains all of the test data for each mean and std measurement
+testY # Contains all of the test activity index data
+
+activityLabel  # Contains the index to string factor pair
+activityFactor # Contains the activity factor for each observation in trainX and testX
+
+subjectTrainLabels # Contains the subject IDs for the training data
+subjectTestLabels  # Contains the subject IDs for the test data
+
+rawData  # Contains the 'raw' data. This matrix is a combination of subject, activity, and trainX/testX data and labels
+tidyData # Contains the average 'mean and std' for each activity for each subject. This variable is also outputted to a file entitled "TidyData.txt".
+'''
+
+
+
---
+++ run_analysis.R
@@ -0,0 +1,80 @@
+####################################################################
+# run_analysis
+#   Desc: Will extract and merge the mean and standard deviation 
+#         values from the training and testing data sets from select
+#         cell phone sensors during a variet of activities.
+#
+#   Author: Kevin O'Neill
+#   Date: 2015.02.22
+#
+#   Usage: 
+#
+#   run_analysis
+#         This script assumes that all of the necessary files are in
+#         the current working direcrtory. Please make sure the 
+#         X_test, X_train, Y_test, Y_train, features, activity_labels,
+#         subject_test, and subject_train .txt files are in the 
+#         working directory.
+#
+####################################################################
+
+## Load Libraries ##
+library(data.table)
+library(dplyr)
+
+## Main Code ##
+
+# File Names for needed files.
+xTrainName = "X_train.txt" # Training Data
+yTrainName = "Y_train.txt" # Activity Data
+xTestName  = "X_test.txt"  # Training Data
+yTestName  = "Y_test.txt"  # Activity Data
+
+subjectTrainName = "subject_train.txt" # Training subjects
+subjectTestName = "subject_test.txt"   # Test subjects
+
+activityName = "activity_labels.txt"   # Activity Label Names
+featureName = "features.txt"           # Feature Label Names
+
+# Indicies for all mean and std values in dataset
+featureColumns = as.numeric(c(1:6, 41:46, 81:86, 121:126, 161:166, 201:202, 214:215, 227:228, 240:241,
+                   266:271, 345:350, 424:429, 503:504, 516:517, 529:530, 524:543))              
+                  
+# Load in feature labels, then alter the feature names to be easier to read
+featureLabels = data.table(read.table(featureName, stringsAsFactors = FALSE))             
+featureLabels = gsub("\\(\\)", "", gsub("-", ".", featureLabels[featureColumns,V2]))
+
+# Load in training data, select mean/std columns, changes names to feature labels
+trainX = data.table(read.table(xTrainName, stringsAsFactors = FALSE))
+trainX = trainX[,featureColumns, with = FALSE] # Select columns
+trainX = setnames(trainX, featureLabels) # Set names to feature names
+
+# Load in test data, select mean/std columns, changes names to feature labels
+testX  = data.table(read.table(xTestName, stringsAsFactors = FALSE))
+testX  = testX[,featureColumns, with = FALSE] # Select columns
+testX  = setnames(testX, featureLabels) # Set names to feature names
+
+# Load in Activity data for both training and test
+trainY = data.table(read.table(yTrainName, stringsAsFactors = FALSE))
+testY  = data.table(read.table(yTestName,  stringsAsFactors = FALSE))
+
+# Generate activity label names from the activity data
+activityLabel = data.table(read.table(activityName, stringsAsFactors = FALSE))
+activityFactor = factor(c(trainY[,V1], testY[,V1]), labels = activityLabel[,V2]) # Create an activity factor vector
+
+# Load in subject data for training and test data
+subjectTrainLabels = data.table(read.table(subjectTrainName, stringsAsFactors = FALSE))
+subjectTestLabels  = data.table(read.table(subjectTestName,  stringsAsFactors = FALSE))
+
+# Combine subject data, activity factor, and mean/std data to form a large data matrix
+rawData = cbind(rbind(subjectTrainLabels,subjectTestLabels), activityFactor, rbind(trainX,testX))
+rawData = setnames(rawData, c(1,2), c("Subject", "Activity")) # Set names of subject and activity columns
+
+# Compute the mean of the data for each activity for each subject
+tidyData = rawData[, lapply(.SD, mean), by = list(Subject, Activity)]
+tidyData = tidyData[order(Subject, Activity)] # Order the data by subject then activity
+
+# Output tidy data to txt file.
+write.table(tidyData, file="TidyData.txt", row.name=FALSE)
+
+# EOF
